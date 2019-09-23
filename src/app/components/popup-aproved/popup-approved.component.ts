710f9eb49b242051a6b6a3d037f2cc28ff81c76b
Antonio Sierra <asierra@fingeosel.com>
2019-09-23 14:44:28 -0500

    landscape page PWA

--- src/app/components/popup-aproved/popup-approved.component.ts
+++ src/app/components/popup-aproved/popup-approved.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import * as reducers from '../../reducers/reducers';
+import { ToggleBlurPage } from '../../actions/platform.actions';
 @Component({
-  selector: 'app-popup',
+  selector: 'app-popup-approved',
   templateUrl: './popup-approved.component.html',
   styleUrls: ['./popup-approved.component.scss']
 })
 export class PopupApprovedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<reducers.State>) { }
 
   ngOnInit() {
+    this.store.dispatch(new ToggleBlurPage())
   }
 
 }
