acc62d73b8e7041eb2a9bdf9da9c1411960335a1
Cesar Ariza Lafaurie <cariza@fingeosel.com>
2021-01-18 15:15:27 -0500

    se agregaron las validaciones que debe cumplir el password, tambien se muestra el mensaje cuando el otp no es valido

--- src/app/pages/signing/signing.component.ts
+++ src/app/pages/signing/signing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import * as reducers from '../../reducers/reducers';
 import { Store } from '@ngrx/store';
@@ -11,12 +11,16 @@ import { CreditsService } from '../../services/credits/credits.service';
   styleUrls: ['./signing.component.scss']
 })
 export class SigningComponent implements OnInit {
+
   listRequest$ = this.store.select(reducers.getListRequestResponse);
 
   tapSigning: number;
   otpForm: FormGroup;
+  signingForm: FormGroup;
   inforequest: any;
   notfound: boolean;
+  isLoading: boolean = false;
+  textError: string;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -24,11 +28,18 @@ export class SigningComponent implements OnInit {
     private store: Store<reducers.State>,
     private creditService: CreditsService
   ) {
-    this.tapSigning = 1;
+    this.tapSigning = 2;
     this.notfound = false;
+    // form otp
     this.otpForm = this.formBuilder.group({
       otp: ['', Validators.required]
     });
+    // form contraseña firma
+    this.signingForm = this.formBuilder.group({
+      conditions: ['', Validators.requiredTrue],
+      contrasena: ['', [Validators.required, Validators.maxLength(15), Validators.minLength(8), this.numberValid,  this.lowercaseUppercaseValid]],
+      confirmcontrasena: ['', Validators.required]
+    }, { validator: this.confirmPassword });
   }
   
   ngOnInit() {
@@ -38,13 +49,18 @@ export class SigningComponent implements OnInit {
   }
   validarOtp() {
     if (this.otpForm.valid) {
+      this.isLoading = true;
       this.notfound = false;
       this.creditService.validateOtp({'cod-otp':this.otpForm.value.otp}).subscribe(data => {
         console.log(data.detail)
+        this.tapSigning = 2;
+        this.isLoading = false;
       }, err => {
         this.notfound = true;
-        console.log(err.error.detail);
+        this.textError = err.error.detail;
+        console.log(this.textError);
       })
+      this.isLoading = false;
     }
 
   }
@@ -60,5 +76,53 @@ export class SigningComponent implements OnInit {
   get code() {
     return this.otpForm.get('otp');
   }
+  get contra() {
+    return this.signingForm.get('contrasena');
+  }
+  get confContra() {
+    return this.signingForm.get('confirmcontrasena');
+  }
+  get condiciones() {
+    return this.otpForm.get('conditions');
+  }
 
+  /**
+   * 
+   * @param group Es el formulario en el que se esta haciendo la validación
+   */
+  confirmPassword(group: FormGroup) {
+    const pass = group.controls.contrasena.value;
+    const confirmpass = group.controls.confirmcontrasena.value;
+    if (pass !== confirmpass) {
+      group.controls.confirmcontrasena.setErrors({ notSame: true });
+    }
+  }
+
+  /**
+   * 
+   * @param control El control sobre el cual se esta haciendo la validación
+   */
+  numberValid(control: FormControl): { [s: string]: boolean } {
+    const mayuscula = new RegExp('.*[0-9].*');
+    if (!control.value.match(mayuscula) && control.value !== '') {
+      return { notNumber: true };
+    }
+    return null;
+  }
+
+  lowercaseUppercaseValid(control: FormControl): { [s: string]: boolean } {
+    const mayuscula = new RegExp('.*[a-zA-Z].*');
+    if (!control.value.match(mayuscula) && control.value !== '') {
+      return { notLowerUpper: true };
+    }
+    return null;
+  }
+
+  // repeatLetter(control: FormControl): { [s: string]: boolean } {
+  //   const repeat = new RegExp('.*([a-z])\1\1\1\1.*');
+  //   if (!control.value.match(repeat) && control.value !== '') {
+  //     return { notRepite: true };
+  //   }
+  //   return null;
+  // }
 }
