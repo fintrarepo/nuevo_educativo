5f1f137e50594560b19001c27de139aaf19b1043
SHIRLEY  MUELLE CAMARGO <62040229+SJmuelle@users.noreply.github.com>
2022-05-23 08:08:01 -0500

    cambios de serivio de simulador para que gusrde la ruta de donde proviene

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   // apiPath: "http://piloto.fintra.co:8084/fintracredit",
   // apiPath: "http://piloto.fintra.co:8084/fintracredit",
 
-  apiPath: "https://prometheus.fintra.co:8443/fintracredit",
+  apiPath: "http://localhost:8084/fintracredit",
   // apiPath: "http://192.168.140.197:8084/fintracredit",
 
   // apiPath:"https://educativofintra.ngrok.io/fintracredit",
